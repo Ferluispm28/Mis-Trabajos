2c5a7151db92980e3fc7e9cc13649c6737dbf02f
Ferluispm28 <luisperesmarrugo28@gmail.com>
2023-11-11 11:07:41 -0500

    suma

--- Mis-Trabajos/ej1.js
+++ Mis-Trabajos/ej1.js
@@ -1,4 +1,7 @@
-let numero=document.getElementById('numero').value;
-function elevar(){
-     
-}
+function calcularSuma(){
+    let a=parseInt(document.getElementById('a').value);
+    let b=parseInt(document.getElementById('b').value);
+
+    let resultado=(a,b)=>a+b;
+    alert(a+b);
+};
